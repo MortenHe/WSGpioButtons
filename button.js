79e12ec4e3c8208444c5e2e5d1c6bcd52cf9c1a7
Martin Helfer <martin-helfer@gmx.de>
2019-10-13 23:23:27 +0200

    first commit

---
+++ button.js
@@ -0,0 +1,53 @@
+//Mit WebsocketServer verbinden
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://localhost:8080');
+
+//GPIO Bibliothek laden
+const Gpio = require('onoff').Gpio;
+
+//Previous-Button
+const buttonPrevious = new Gpio(160, 'in', 'rising', { debounceTimeout: 10 });
+
+//Pause-Button
+const buttonPause = new Gpio(253, 'in', 'rising', { debounceTimeout: 10 });
+
+//Next-Button
+const buttonNext = new Gpio(252, 'in', 'rising', { debounceTimeout: 10 });
+
+//Wenn Verbindung mit WSS hergestellt wird
+ws.on('open', function open() {
+    console.log("connected to wss");
+
+    //Wenn Button gedrueckt wird -> vorherigen Titel abspielen
+    buttonPrevious.watch(function (err, value) {
+        console.log("previous track");
+
+        //Nachricht an WSS schicken
+        ws.send(JSON.stringify({
+            type: "change-item",
+            value: false
+        }));
+    });
+
+    //Wenn Button gedrueckt wird -> Pause / Unpuase
+    buttonPause.watch(function (err, value) {
+        console.log("toggle paused");
+
+        //Nachricht an WSS schicken
+        ws.send(JSON.stringify({
+            type: "toggle-paused-restart",
+            value: ""
+        }));
+    });
+
+    //Wenn Button gedrueckt wurd -> naechsten Titel abspielen
+    buttonNext.watch(function (err, value) {
+        console.log("next track");
+
+        //Nachricht an WSS schicken
+        ws.send(JSON.stringify({
+            type: "change-item",
+            value: true
+        }));
+    });
+});
